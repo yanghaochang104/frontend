0b366499f5728bb011a111399f39bd7a265a192d
zztin <litingchen16@gmail.com>
2022-01-12 15:21:37 +0100

    更改免責聲明及CC授權

--- src/components/GettingStartedModal.vue
+++ src/components/GettingStartedModal.vue
@@ -7,13 +7,13 @@
         <p>需要大家回報以下黑熊出沒痕跡之照片及資訊：</p>
 
         <small class="text-center" style="padding-left: 20px; padding-right: 20px;">
-          行動前請同意資料貢獻<a href="https://license.disfactory.tw/#%E8%BE%B2%E5%9C%B0%E9%81%95%E7%AB%A0%E5%B7%A5%E5%BB%A0%E5%9B%9E%E5%A0%B1%E7%B3%BB%E7%B5%B1%EF%BC%88disfactory%EF%BC%89%E8%B2%A2%E7%8D%BB%E5%8D%94%E8%AD%B0" target="_blank">授權條款</a>與<a href="https://license.disfactory.tw/#%E9%9A%B1%E7%A7%81%E6%AC%8A%E6%A2%9D%E6%AC%BE" target="_blank">隱私條款</a>。本網站由<a href="https://www.cet-taiwan.org/" target="_blank">地球公民基金會</a>與 <a href="https://g0v.tw/" target="_blank">g0v.tw</a> 社群共同開發。
+          本會(台灣黑熊保護協會)取得您的個人資料，在個人資料保護法及相關法令之規定下，依本處隱私權保護政策，蒐集、處理及利用您的個人資料。 您亦同意本處以您所提供之個人資料確認您的身分、與您進行連絡，以及其他隱私權保護政策規範之使用方式。本人同意本人於Oh Shown!上傳之所有內容，採用創用 CC 姓名標示4.0版台灣授權條款在台灣黑熊保護協會公開分享以及無償作為學術單位、政府單位使用。 並了解本授權條款係適用於全世界，且其有效期間持續至本著作的著作權保護期間屆滿為止，同時具不可撤回之特性。 本人亦了解他人合理使用的權利及其他相關規範，不因本授權條款之內容而受影響。 本人擔保本作品未侵害或抄襲他人之著作（作品中使用他人作品之部份，包含圖像及聲音等），若有違反智慧財產之情事者，願自行負擔一切法律責任。 創用CC台灣授權條款詳見 <a href="http://creativecommons.tw/license" target="_blank">此處</a>。本網站由 <a href="https://g0v.tw/" target="_blank">g0v.tw</a> 社群開發。
         </small>
 
         <app-button @click="agree">我同意加入回報</app-button>
 
         <small>
-          <a href="https://about.disfactory.tw">我不同意，請帶我離開這個頁面</a>
+          <a href="https://www.taiwanbear.org.tw/">我不同意，請帶我離開這個頁面</a>
         </small>
       </div>
     </app-modal>
